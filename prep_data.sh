4f7a326aa7767098e02bc63b49fc1dfb979effe7
Hoang Minh Long <longgg11245@gmail.com>
2025-10-24 10:03:42 +0700

    add repare data

--- prep_data.sh
+++ prep_data.sh
@@ -14,6 +14,9 @@ mkdir -p dataset
 cd dataset
 
 pip install gdown librosa speechbrain jiwer
+pip install https://huggingface.co/csukuangfj/k2/resolve/main/ubuntu-cuda/k2-1.24.4.dev20250807+cuda12.8.torch2.8.0-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl
+pip install git+https://github.com/lhotse-speech/lhotse
+
 gdown 19CV4WZgYez-i2oHV2r9maJofjNqcTX4o 
 gdown 1v75mLO-TVfPXe27o54JMlXD5cQ81eaVG 
 gdown 1YgTF-NbHuweHWr2LahS_X9j--laGDnIK
